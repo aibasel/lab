f8593eb64753f337149eba460099e14a84668fe5
Jendrik Seipp <jendrik.seipp@unibas.ch>
2011-12-15 17:20:27 +0100

    use base scripts dir in run-template.py

--- data/run-template.py
+++ data/run-template.py
@@ -7,11 +7,11 @@ import os
 # make sure we're in the run directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-calls_dir = os.path.abspath("../../lab/calls")
-sys.path.insert(0, calls_dir)
+lab_dir = os.path.abspath("../../lab")
+sys.path.insert(0, lab_dir)
 
-from call import Call
-from log import print_, redirects, save_returncode, driver_log, driver_err
+from calls.call import Call
+from calls.log import print_, redirects, save_returncode, driver_log, driver_err
 
 sys.stdout = driver_log
 sys.stderr = driver_err
