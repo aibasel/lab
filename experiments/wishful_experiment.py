6f1c2c4d83f271f25e99069a7af85f628c206fa8
Jendrik Seipp <jendrik.seipp@unibas.ch>
2011-12-15 15:37:32 +0100

    cosmetic changes to test exp

--- experiments/wishful_experiment.py
+++ experiments/wishful_experiment.py
@@ -3,9 +3,7 @@
 """
 Questions:
 
-How do we want to do reports?
 How do we add values like "quality"?
-Split generic and downward experiment code?
 """
 
 import os
@@ -14,7 +12,6 @@ import sys
 
 sys.path.insert(0, '/home/jendrik/projects/Downward/lab/')
 
-# Use the full paths here to demonstrate the layout
 # Later we could use __init__.py files to simplify things a bit
 from lab.downward.downward_experiment import DownwardExperiment
 from lab.downward.checkouts import Translator, Preprocessor, Planner
@@ -28,14 +25,16 @@ from lab.downward import configs
 from lab.experiment import Step
 from lab import tools
 
-EXPNAME = 'myexp'
+EXPNAME = 'test-exp'
+EXPPATH = os.path.join(tools.USER_DIR, 'experiments', EXPNAME)
 REPORTS = os.path.join(tools.USER_DIR, 'reports')
 REPO = '/home/jendrik/projects/Downward/downward'
+ATTRIBUTES = ['cost', 'coverage', 'search_time']
 
 combinations = [(Translator(repo=REPO), Preprocessor(repo=REPO), Planner(repo=REPO))]
 
-exp = DownwardExperiment(path='/home/jendrik/%s' % EXPNAME, env=LocalEnvironment(),
-                         repo=REPO, combinations=combinations)
+exp = DownwardExperiment(path=EXPPATH, env=LocalEnvironment(), repo=REPO,
+                         combinations=combinations)
 
 exp.add_suite('gripper:prob01.pddl')
 exp.add_suite('depot:pfile1')
@@ -45,26 +44,23 @@ exp.add_config('yY', configs.yY)
 exp.add_config('lama', configs.lama)
 exp.add_config('multiple', configs.multiple_plans)
 exp.add_config('iterated', configs.iterated_search)
-#for config in configs.ipc_optimal_subset():
-#    exp.add_config(*config)
 
 def lama_and_yY(run):
     return run['config'] in ['WORK-lama', 'WORK-yY']
 
-attributes = ['cost', 'coverage', 'search_time']
 abs_domain_report_file = os.path.join(REPORTS, '%s-abs-d.html' % EXPNAME)
 abs_problem_report_file = os.path.join(REPORTS, '%s-abs-p.html' % EXPNAME)
 #exp.add_report(AbsoluteReport, outfile=abs_report_file)
-exp.add_step(Step('report-abs-d', AbsoluteReport('domain', attributes=attributes), exp.eval_dir, abs_domain_report_file))
-exp.add_step(Step('report-abs-p', AbsoluteReport('problem', attributes=attributes), exp.eval_dir, abs_problem_report_file))
-exp.add_step(Step('report-ipc', IpcReport('coverage'), exp.eval_dir, os.path.join(REPORTS, 'ipc.tex')))
+exp.add_step(Step('report-abs-d', AbsoluteReport('domain', attributes=ATTRIBUTES), exp.eval_dir, abs_domain_report_file))
+exp.add_step(Step('report-abs-p', AbsoluteReport('problem', attributes=ATTRIBUTES), exp.eval_dir, abs_problem_report_file))
+exp.add_step(Step('report-ipc', IpcReport(attributes=['coverage']), exp.eval_dir, os.path.join(REPORTS, 'ipc.tex')))
 exp.add_step(Step('report-scatter', ScatterPlotReport(attributes=['expansions'], filters=[lama_and_yY]), exp.eval_dir, os.path.join(REPORTS, 'scatter.png')))
 exp.add_step(Step('report-suite', SuiteReport(), exp.eval_dir, os.path.join(REPORTS, 'suite.py')))
 exp.add_step(Step('report-rel', RelativeReport('problem', filters=[lama_and_yY]), exp.eval_dir, os.path.join(REPORTS, 'rel.html')))
 
 # exp.steps is a list that can be manipulated:
 # steps can be removed, appended, replaced and inserted
-#exp.steps.insert(6, Step('scatter-report', ScatterReport(), dir=exp.eval_dir))
+#exp.steps.insert(6, Step('report-abs', AbsoluteReport('domain', attributes=['coverage']), exp.eval_dir, 'report-abs.html'))
 #del exp.steps[7]
 
 # Compress the experiment directory
