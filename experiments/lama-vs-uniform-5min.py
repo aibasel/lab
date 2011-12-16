3c477d7b4accd25579dc7b8bda22fc67b0171c6a
Jendrik Seipp <jendrik.seipp@unibas.ch>
2011-12-16 19:14:41 +0100

    add lama11 vs uniform portfolio exp with 5min search time

--- experiments/lama-vs-uniform-5min.py
+++ experiments/lama-vs-uniform-5min.py
@@ -7,58 +7,53 @@ How do we add values like "quality"?
 """
 
 import os
+import platform
 import shutil
 import sys
 
 from lab.downward.downward_experiment import DownwardExperiment
 from lab.downward.checkouts import Translator, Preprocessor, Planner
 from lab.downward.reports.absolute import AbsoluteReport
-from lab.downward.reports.ipc import IpcReport
-from lab.downward.reports.scatter import ScatterPlotReport
-from lab.downward.reports.suite import SuiteReport
-from lab.downward.reports.relative import RelativeReport
-from lab.environments import LocalEnvironment
+from lab.environments import LocalEnvironment, GkiGridEnvironment
 from lab.downward import configs
 from lab.experiment import Step
 from lab import tools
 
 
-EXPNAME = 'test-exp'
-EXPPATH = os.path.join(tools.USER_DIR, 'experiments', EXPNAME)
-REPORTS = os.path.join(tools.USER_DIR, 'reports')
-REPO = '/home/jendrik/projects/Downward/downward'
-ATTRIBUTES = ['cost', 'coverage', 'search_time']
-LIMITS = {'search_time': 900}
+EXPNAME = os.path.splitext(os.path.basename(__file__))[0]
+if platform.node() == 'habakuk':
+    EXPPATH = os.path.join('/home/downward/jendrik/experiments/', EXPNAME)
+    REPORTS = '/home/downward/jendrik/reports'
+    REPO = '/home/downward/jendrik/fastr'
+    SUITE = 'IPC11'
+    ENV = GkiGridEnvironment()
+    PORTFOLIOS = '/home/downward/jendrik/fastr/new-scripts/portfolios'
+else:
+    EXPPATH = os.path.join(tools.DEFAULT_EXP_DIR, EXPNAME)
+    REPORTS = tools.DEFAULT_REPORTS_DIR
+    REPO = '/home/jendrik/projects/Downward/fastr'
+    SUITE = 'gripper:prob01.pddl'
+    ENV = LocalEnvironment()
+    PORTFOLIOS = '/home/jendrik/projects/Downward/fastr/new-scripts/portfolios'
+
+ATTRIBUTES = ['cost', 'coverage']
+LIMITS = {'search_time': 300}
 COMBINATIONS = [(Translator(repo=REPO), Preprocessor(repo=REPO), Planner(repo=REPO))]
-ENV = LocalEnvironment()
 
 exp = DownwardExperiment(path=EXPPATH, env=ENV, repo=REPO,
                          combinations=COMBINATIONS, limits=LIMITS)
 
-exp.add_suite('gripper:prob01.pddl')
-exp.add_suite('depot:pfile1')
+exp.add_suite(SUITE)
 
-exp.add_config(*configs.pdb_max_states(1000))
-exp.add_config('yY', configs.yY)
-exp.add_config('lama', configs.lama)
-exp.add_config('multiple', configs.multiple_plans)
-exp.add_config('iterated', configs.iterated_search)
-exp.add_portfolio(os.path.join(REPO, 'src/search/downward-seq-sat-fdss-1.py'))
-exp.add_portfolio('/home/jendrik/projects/Downward/fastr/new-scripts/portfolios/uniform.py')
+exp.add_config('lama11', configs.lama11)
+exp.add_portfolio(os.path.join(PORTFOLIOS, 'uniform-5min.py'))
 
-exp.steps.insert(5, Step('clear-results', shutil.rmtree, exp.eval_dir))
-
-def lama_and_yY(run):
-    return run['config'] in ['WORK-lama', 'WORK-yY']
+#exp.steps.insert(5, Step('clear-results', shutil.rmtree, exp.eval_dir))
 
 abs_domain_report_file = os.path.join(REPORTS, '%s-abs-d.html' % EXPNAME)
 abs_problem_report_file = os.path.join(REPORTS, '%s-abs-p.html' % EXPNAME)
 exp.add_step(Step('report-abs-d', AbsoluteReport('domain', attributes=ATTRIBUTES), exp.eval_dir, abs_domain_report_file))
 exp.add_step(Step('report-abs-p', AbsoluteReport('problem', attributes=ATTRIBUTES), exp.eval_dir, abs_problem_report_file))
-#exp.add_step(Step('report-ipc', IpcReport(attributes=['coverage']), exp.eval_dir, os.path.join(REPORTS, 'ipc.tex')))
-#exp.add_step(Step('report-scatter', ScatterPlotReport(attributes=['expansions'], filters=[lama_and_yY]), exp.eval_dir, os.path.join(REPORTS, 'scatter.png')))
-#exp.add_step(Step('report-suite', SuiteReport(), exp.eval_dir, os.path.join(REPORTS, 'suite.py')))
-#exp.add_step(Step('report-rel', RelativeReport('problem', filters=[lama_and_yY]), exp.eval_dir, os.path.join(REPORTS, 'rel.html')))
 
 # exp.steps is a list that can be manipulated:
 # steps can be removed, appended, replaced and inserted
