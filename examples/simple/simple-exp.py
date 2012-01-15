54304b6fd6bf5f7ef41390d3c9f9b27b36a510e1
Jendrik Seipp <jendrik.seipp@unibas.ch>
2012-01-15 17:30:34 +0100

    make simple report in eval dir

--- examples/simple/simple-exp.py
+++ examples/simple/simple-exp.py
@@ -14,15 +14,14 @@ from lab import tools
 EXPNAME = 'simple-exp'
 if platform.node() == 'habakuk':
     EXPPATH = os.path.join('/home/downward/jendrik/experiments/', EXPNAME)
-    REPORT = os.path.join('/home/downward/jendrik/reports', EXPNAME + '.html')
     ENV = GkiGridEnvironment()
 else:
     EXPPATH = os.path.join(tools.DEFAULT_EXP_DIR, EXPNAME)
-    REPORT = os.path.join(tools.DEFAULT_REPORTS_DIR, 'simple-report.html')
     ENV = LocalEnvironment()
 
 exp = Experiment(path=EXPPATH, env=ENV)
 exp.add_resource('SIMPLE_PARSER', 'simple-parser.py', 'simple-parser.py')
+reportfile = os.path.join(exp.eval_dir, EXPNAME + '.html')
 
 run = exp.add_run()
 run.add_command('list-dir', ['ls'])
@@ -31,12 +30,12 @@ run.require_resource('SIMPLE_PARSER')
 run.add_command('parse', ['SIMPLE_PARSER'])
 
 # Make a default report
-exp.add_step(Step('report', Report(), exp.eval_dir, REPORT))
+exp.add_step(Step('report', Report(), exp.eval_dir, reportfile))
 
 # Compress the experiment directory
 exp.add_step(Step.zip_exp_dir(exp))
 
-exp.add_step(Step.publish_reports(REPORT))
+exp.add_step(Step.publish_reports(reportfile))
 
 # Remove the experiment directory
 exp.add_step(Step.remove_exp_dir(exp))
