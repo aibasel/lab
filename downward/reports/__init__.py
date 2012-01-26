22f390bafb2f98fdcdb04c0c69afd179acf709de
Jendrik Seipp <jendrik.seipp@unibas.ch>
2012-01-26 01:54:02 +0100

    add single_solver attribute

--- downward/reports/__init__.py
+++ downward/reports/__init__.py
@@ -43,11 +43,19 @@ def quality(problem_runs):
         run['quality'] = round(quality, 4)
 
 
+def single_solver(problem_runs):
+    solvers = sum(run.get('coverage') for run in problem_runs)
+    for run in problem_runs:
+        single_solver = (solvers == 1 and run.get('coverage') == 1)
+        run['single_solver'] = int(single_solver)
+
+
 class PlanningReport(Report):
     def __init__(self, *args, **kwargs):
         self.derived_properties = kwargs.pop('derived_properties', [])
         Report.__init__(self, *args, **kwargs)
         self.derived_properties.append(quality)
+        self.derived_properties.append(single_solver)
 
     def scan_data(self):
         self.scan_planning_data()
@@ -106,7 +114,7 @@ class PlanningReport(Report):
         '''
         # Decide whether we want to highlight minima or maxima
         max_attribute_parts = ['score', 'initial_h_value', 'coverage',
-                               'quality']
+                               'quality', 'single_solver']
         min_wins = True
         for attr_part in max_attribute_parts:
             if attr_part in attribute:
