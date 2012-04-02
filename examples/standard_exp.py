2528339289e1c7a60b7cacb75faf7758291a446a
Jendrik Seipp <jendrik.seipp@unibas.ch>
2012-04-02 14:47:38 +0200

    let StandardDownwardExperiment inherit from DownwardExperiment

--- examples/standard_exp.py
+++ examples/standard_exp.py
@@ -25,27 +25,35 @@ else:
 ATTRIBUTES = ['coverage', 'cost', 'total_time', 'single_solver']
 
 
+class StandardDownwardExperiment(DownwardExperiment):
+    def __init__(self, path=EXPPATH, environment=ENV, repo=REPO,
+                 combinations=None, limits=None):
+        DownwardExperiment.__init__(self, path=path, environment=environment,
+                                    repo=repo, combinations=combinations,
+                                    limits=limits)
+
+        # Add report steps
+        abs_domain_report_file = os.path.join(self.eval_dir, '%s-abs-d.html' % EXPNAME)
+        abs_problem_report_file = os.path.join(self.eval_dir, '%s-abs-p.html' % EXPNAME)
+        self.add_step(Step('report-abs-d', AbsoluteReport('domain', attributes=ATTRIBUTES),
+                                                          self.eval_dir, abs_domain_report_file))
+        self.add_step(Step('report-abs-p', AbsoluteReport('problem', attributes=ATTRIBUTES),
+                                                          self.eval_dir, abs_problem_report_file))
+
+        # Copy the results
+        self.add_step(Step.publish_reports(abs_domain_report_file, abs_problem_report_file))
+
+        # Compress the experiment directory
+        self.add_step(Step.zip_exp_dir(self))
+
+
 def get_exp(suite, configs, combinations=None, limits=None):
     # Test configs on local machine
     if platform.node() != 'habakuk':
         suite = 'gripper:prob01.pddl'
 
-    exp = DownwardExperiment(path=EXPPATH, environment=ENV, repo=REPO,
-                             combinations=combinations, limits=limits)
-
-    # Add report steps
-    abs_domain_report_file = os.path.join(exp.eval_dir, '%s-abs-d.html' % EXPNAME)
-    abs_problem_report_file = os.path.join(exp.eval_dir, '%s-abs-p.html' % EXPNAME)
-    exp.add_step(Step('report-abs-d', AbsoluteReport('domain', attributes=ATTRIBUTES),
-                                                     exp.eval_dir, abs_domain_report_file))
-    exp.add_step(Step('report-abs-p', AbsoluteReport('problem', attributes=ATTRIBUTES),
-                                                     exp.eval_dir, abs_problem_report_file))
-
-    # Copy the results
-    exp.add_step(Step.publish_reports(abs_domain_report_file, abs_problem_report_file))
-
-    # Compress the experiment directory
-    exp.add_step(Step.zip_exp_dir(exp))
+    exp = StandardDownwardExperiment(path=EXPPATH, environment=ENV, repo=REPO,
+                                     combinations=combinations, limits=limits)
 
     exp.add_suite(suite)
     for nick, config in configs:
