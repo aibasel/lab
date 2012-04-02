e6ef9e22c5062b3a216ede01beb56ee60ec8ffb4
Jendrik Seipp <jendrik.seipp@unibas.ch>
2012-04-02 15:35:09 +0200

    allow specifying attributes in standard exp

--- examples/standard_exp.py
+++ examples/standard_exp.py
@@ -22,22 +22,25 @@ else:
     REPO = '/home/jendrik/projects/Downward/downward'
     ENV = LocalEnvironment()
 
-ATTRIBUTES = ['coverage', 'cost', 'total_time', 'single_solver']
+ATTRIBUTES = ['coverage', 'cost', 'total_time']
 
 
 class StandardDownwardExperiment(DownwardExperiment):
     def __init__(self, path=EXPPATH, environment=ENV, repo=REPO,
-                 combinations=None, limits=None):
+                 combinations=None, limits=None, attributes=None):
         DownwardExperiment.__init__(self, path=path, environment=environment,
                                     repo=repo, combinations=combinations,
                                     limits=limits)
 
+        if attributes is None:
+            attributes = ATTRIBUTES
+
         # Add report steps
         abs_domain_report_file = os.path.join(self.eval_dir, '%s-abs-d.html' % EXPNAME)
         abs_problem_report_file = os.path.join(self.eval_dir, '%s-abs-p.html' % EXPNAME)
-        self.add_step(Step('report-abs-d', AbsoluteReport('domain', attributes=ATTRIBUTES),
+        self.add_step(Step('report-abs-d', AbsoluteReport('domain', attributes=attributes),
                                                           self.eval_dir, abs_domain_report_file))
-        self.add_step(Step('report-abs-p', AbsoluteReport('problem', attributes=ATTRIBUTES),
+        self.add_step(Step('report-abs-p', AbsoluteReport('problem', attributes=attributes),
                                                           self.eval_dir, abs_problem_report_file))
 
         # Copy the results
@@ -47,13 +50,14 @@ class StandardDownwardExperiment(DownwardExperiment):
         self.add_step(Step.zip_exp_dir(self))
 
 
-def get_exp(suite, configs, combinations=None, limits=None):
+def get_exp(suite, configs, combinations=None, limits=None, attributes=None):
     # Test configs on local machine
     if platform.node() != 'habakuk':
         suite = 'gripper:prob01.pddl'
 
     exp = StandardDownwardExperiment(path=EXPPATH, environment=ENV, repo=REPO,
-                                     combinations=combinations, limits=limits)
+                                     combinations=combinations, limits=limits,
+                                     attributes=attributes)
 
     exp.add_suite(suite)
     for nick, config in configs:
