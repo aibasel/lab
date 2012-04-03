e2c55c0ae524b619e9d64c318cbee75027c16a07
Jendrik Seipp <jendrik.seipp@unibas.ch>
2012-04-03 16:35:52 +0200

    Handle KeyboardInterrupt better for local experiments

--- data/local-job-template.py
+++ data/local-job-template.py
@@ -9,28 +9,36 @@ import subprocess
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
-def process_dir(dir):
-    try:
-        number = dir.split('/')[-1]
-        print 'Starting run %s/%s' % (number, str(len(dirs)).zfill(5))
-        run = subprocess.Popen(['./run'], cwd=dir, stdout=sys.stdout, stderr=sys.stderr)
-        run.wait()
-    except KeyboardInterrupt:
-        print 'Call to run interrupted'
-        run.terminate()
-    except OSError, err:
-        print err
-
 dirs = [
 """DIRS"""
 ]
 
-pool = multiprocessing.Pool(processes="""PROCESSES""")
-res = pool.map_async(process_dir, dirs)
-pool.close()
 
-try:
-    pool.join()
-except KeyboardInterrupt:
-    print 'Main script interrupted'
-    pool.terminate()
+def process_dir(dir):
+    number = dir.split('/')[-1]
+    print 'Starting run %s/%s' % (number, str(len(dirs)).zfill(5))
+    run = subprocess.Popen(['./run'], cwd=dir, stdout=sys.stdout, stderr=sys.stderr)
+    try:
+        run.wait()
+    except KeyboardInterrupt:
+        print 'Call to run %s interrupted' % number
+        run.terminate()
+    except OSError, err:
+        print err
+
+
+def main():
+    pool = multiprocessing.Pool(processes="""PROCESSES""")
+    try:
+        pool.map(process_dir, dirs)
+        pool.close()
+    except KeyboardInterrupt:
+        print 'Main script interrupted'
+        pool.terminate()
+    finally:
+        print 'Joining pool processes'
+        pool.join()
+
+
+if __name__ == '__main__':
+    main()
