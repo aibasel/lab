b13958d48bdc124451ab9ef539235039906ea1a6
Florian Pommerening <florian.pommerening@unibas.ch>
2012-07-19 14:48:38 +0200

    Inherit ScatterPlot from Planning Report instead of AbsoluteReport. Additional example Default category -> None tuple_category -> get_category value_groups -> categories

--- downward/reports/scatter.py
+++ downward/reports/scatter.py
@@ -26,34 +26,55 @@ from collections import defaultdict
 
 from lab import tools
 
-from downward.reports.absolute import AbsoluteReport
+from downward.reports import PlanningReport
 
 
-class ScatterPlotReport(AbsoluteReport):
+class ScatterPlotReport(PlanningReport):
     """
     Generate a scatter plot for a specific attribute.
     """
-    def __init__(self, tuple_category=None, category_styles={}, *args, **kwargs):
+    def __init__(self, get_category=None, category_styles={}, *args, **kwargs):
         """
         ``kwargs['attributes']`` must contain exactly one attribute.
 
-        **tuple_category** can contain a function taking two dictionaries of run
+        *get_category* can contain a function taking two dictionaries of run
         properties and returning a string that will be used to group the values.
+        Values where this function does not return a value (or returns None)
+        are shown in a default category and not contained in the legend.
         For example, to group by domain use::
 
-        def domain_tuple_category(run1, run2):
-          return run1['domain'] # run2['domain'] will have the same value
+            def domain_as_category(run1, run2):
+                return run1['domain'] # run2['domain'] will have the same value
 
-        **category_styles** can be a dictionary that maps category names to tuples
+        *category_styles* can be a dictionary that maps category names to tuples
         (marker, color) where marker and color are valid values for pyplot
         (see http://matplotlib.sourceforge.net/api/pyplot_api.html)
         For example to change the default style to blue stars use::
 
-        ScatterPlotReport(attributes=['time'], category_styles={'DEFAULT': ('*','b')})
+            ScatterPlotReport(attributes=['time'], category_styles={None: ('*','b')})
+
+        *get_category* and *category_styles* are best used together, e.g. to 
+        highlight a domain or interesting values::
+ 
+            def my_categories(run1, run2):
+                if run1['search_time'] > 10 * run2['search_time']:
+                    return 'strong improvement'
+                if run1['domain'] == 'gripper':
+                    return 'gripper'
+
+            my_styles = {
+                'strong improvement': ('x','r'),
+                'gripper': ('*','b'),
+                None: ('o','y'),
+            }
+
+            ScatterPlotReport(attributes=['time'],
+                              get_category=my_categories,
+                              category_styles=my_styles)
         """
-        self.tuple_category = tuple_category
+        self.get_category = get_category
         self.category_styles = category_styles
-        AbsoluteReport.__init__(self, 'problem', *args, **kwargs)
+        PlanningReport.__init__(self, *args, **kwargs)
         assert len(self.attributes) == 1, self.attributes
 
     def write_plot(self, attribute, filename):
@@ -65,50 +86,56 @@ class ScatterPlotReport(AbsoluteReport):
             logging.error('matplotlib could not be found: %s' % err)
             sys.exit(1)
 
-        table = self._get_table(attribute)
-        cfg1, cfg2 = table.cols
-        columns = table.get_columns()
-        assert len(columns[cfg1]) == len(columns[cfg2]), columns
+        attribute = self.attributes[0]
+        all_values = []
+        configs = set()
+        for runs in self.problem_runs.values():
+            for run in runs:
+                all_values.append(run.get(attribute))
+                configs.add(run['config'])
+        # Fix an order of configs
+        configs = list(configs)
+        assert len(configs) == 2
 
         # It may be the case that no values are found
         try:
-            max_value = max(columns[cfg1] + columns[cfg2])
+            max_value = max(all_values)
         except ValueError:
             pass
 
         if max_value is None or max_value <= 0:
             logging.info('Found no valid datapoints for the plot.')
-            print table
+            print all_values
             sys.exit()
 
         # Separate the missing values by plotting them at (value * 10) rounded
         # to the next power of 10.
         missing_val = 10 ** math.ceil(math.log10(max_value * 10))
 
-        # Map category names to tuples of 2 lists (values1, values2).
-        # The lists contain the values for axis 1 and 2 respectively.
-        value_groups = defaultdict(lambda: ([], []))
-        for row, val1, val2 in zip(table.rows, columns[cfg1], columns[cfg2]):
+        # Map category names to value tuples
+        categories = defaultdict(list)
+        for (domain, problem), (run1, run2) in self.problem_runs.items():
+            # Runs can occur in the wrong order
+            if run2['config'] == configs[0]:
+                run1, run2 = run2, run1
+            val1 = run1.get(attribute)
+            val2 = run2.get(attribute)
             if val1 is None and val2 is None:
                 continue
             if val1 is None:
                 val1 = missing_val
             if val2 is None:
                 val2 = missing_val
-            if self.tuple_category is None:
-                category = 'DEFAULT'
+            if self.get_category is None:
+                category = None
             else:
-                domain, problem = row.split(':')
-                run1 = self.runs[(domain, problem, cfg1)]
-                run2 = self.runs[(domain, problem, cfg2)]
-                category = self.tuple_category(run1, run2)
-            value_groups[category][0].append(val1)
-            value_groups[category][1].append(val2)
+                category = self.get_category(run1, run2)
+            categories[category].append((val1, val2))
 
         # Pick any style for categories for which no style is defined.
         # TODO add more possible styles
-        possible_styles = [(m, c) for m in 'ox*+' for c in 'rgby']
-        missing_category_styles = (set(value_groups.keys()) -
+        possible_styles = [(m, c) for m in 'ox+^v<>' for c in 'rgby']
+        missing_category_styles = (set(categories.keys()) -
                                    set(self.category_styles.keys()))
         for i, missing in enumerate(missing_category_styles):
             self.category_styles[missing] = possible_styles[i % len(possible_styles)]
@@ -123,21 +150,21 @@ class ScatterPlotReport(AbsoluteReport):
         ax = fig.add_subplot(111)
 
         # Make a descriptive title and set axis labels
-        title = ' '.join([attribute, 'by', self.resolution])
+        title = attribute
         ax.set_title(title, fontsize=14)
-        ax.set_xlabel(cfg1, fontsize=12)
-        ax.set_ylabel(cfg2, fontsize=12)
+        ax.set_xlabel(configs[0], fontsize=12)
+        ax.set_ylabel(configs[1], fontsize=12)
 
         # Display grid
         ax.grid(b=True, linestyle='-', color='0.75')
 
         # Generate the scatter plots
-        for category, (values1, values2) in sorted(value_groups.items()):
+        for category, coordinates in sorted(categories.items()):
             marker, c = self.category_styles[category]
-            ax.scatter(values1, values2, s=20, marker=marker, c=c, label=category)
+            ax.scatter(*zip(*coordinates), s=20, marker=marker, c=c, label=category)
 
         # Generate a legend if there is at least one non-default category
-        if value_groups.keys() != ['DEFAULT']:
+        if categories.keys() != [None]:
             ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
         # Plot a diagonal black line. Starting at (0,0) often raises errors.
