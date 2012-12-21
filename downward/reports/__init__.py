f11fcc0b2eddd59d4dfd48198a123a87dd9abfa0
Florian Pommerening <florian.pommerening@unibas.ch>
2012-12-21 09:48:27 +0100

    Include table of all unexplained error at the start of absolute reports

--- downward/reports/__init__.py
+++ downward/reports/__init__.py
@@ -196,6 +196,24 @@ class PlanningReport(Report):
                     run_id = '-'.join((run['config'], run['domain'], run['problem']))
                     self.props[run_id] = run
 
+    def _get_warnings_table(self):
+        """
+        Returns a :py:class:`Table <lab.reports.Table>` containing one line for each run
+        where a serious error occured. Every error that is not 'none', 'unsolvable',
+        'timeout' or 'mem-limit-exceeded' is considered serious.
+        """
+        sanctioned_error_reasons = [None, 'none', 'unsolvable',
+                                    'timeout', 'mem-limit-exceeded']
+        columns = ['domain', 'problem', 'config', 'error',
+                   'last_logged_time', 'last_logged_memory']
+        table = reports.Table(title='Unexplained errors')
+        table.set_column_order(columns)
+        for run in self.props.values():
+            if run.get('error') not in sanctioned_error_reasons:
+                for column in columns:
+                    table.add_cell(run['run_dir'], column, run.get(column, '?'))
+        return table
+
     def _get_config_order(self):
         """
         Returns a list of configs in the order that was determined by the user.
