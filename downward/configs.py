7d766bc6ce5826845030abfdd3690dc676cf7917
Jendrik Seipp <jendrik.seipp@unibas.ch>
2013-08-02 23:07:23 +0200

    change config suite names

--- downward/configs.py
+++ downward/configs.py
@@ -17,8 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-# TODO: Convert to lists of pairs and adapt in compare_revisions().
-def configs_core_optimal():
+def configs_optimal_core():
     return {
         # A*
         'astar_blind': [
@@ -56,7 +55,7 @@ def configs_core_optimal():
     }
 
 
-def configs_core_satisficing():
+def configs_satisficing_core():
     return {
         # A*
         'astar_goalcount': [
@@ -102,7 +101,7 @@ def configs_core_satisficing():
     }
 
 
-def configs_ipc_optimal():
+def configs_optimal_ipc():
     return {
         'seq_opt_merge_and_shrink': ['ipc', 'seq-opt-merge-and-shrink'],
         'seq_opt_fdss_1': ['ipc', 'seq-opt-fdss-1'],
@@ -110,7 +109,7 @@ def configs_ipc_optimal():
     }
 
 
-def configs_ipc_satisficing():
+def configs_satisficing_ipc():
     return {
         'seq_sat_lama_2011': ['ipc', 'seq-sat-lama-2011'],
         'seq_sat_fdss_1': ['ipc', 'seq-sat-fdss-1'],
@@ -118,7 +117,7 @@ def configs_ipc_satisficing():
     }
 
 
-def configs_extended_optimal():
+def configs_optimal_extended():
     return {
         # A*
         'astar_lmcount_lm_merged_rhw_hm_no_order': [
@@ -140,7 +139,7 @@ def configs_extended_optimal():
     }
 
 
-def configs_extended_satisficing():
+def configs_satisficing_extended():
     return {
         # eager greedy
         'eager_greedy_alt_ff_cg': [
@@ -198,20 +197,20 @@ def configs_extended_satisficing():
 def default_configs_optimal(core=True, ipc=True, extended=False):
     configs = {}
     if core:
-        configs.update(configs_core_optimal())
+        configs.update(configs_optimal_core())
     if ipc:
-        configs.update(configs_ipc_optimal())
+        configs.update(configs_optimal_ipc())
     if extended:
-        configs.update(configs_extended_optimal())
+        configs.update(configs_optimal_extended())
     return configs
 
 
 def default_configs_satisficing(core=True, ipc=True, extended=False):
     configs = {}
     if core:
-        configs.update(configs_core_satisficing())
+        configs.update(configs_satisficing_core())
     if ipc:
-        configs.update(configs_ipc_satisficing())
+        configs.update(configs_satisficing_ipc())
     if extended:
-        configs.update(configs_extended_satisficing())
+        configs.update(configs_satisficing_extended())
     return configs
