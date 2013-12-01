be899aa2172a76714aeb705dfa56e0cb6328e01c
Jendrik Seipp <jendrik.seipp@unibas.ch>
2013-12-01 22:16:55 +0100

    allow passing final_config(_builder) to portfolio creation function

--- downward/portfolios.py
+++ downward/portfolios.py
@@ -16,10 +16,29 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import inspect
 import json
 import tempfile
 
 from lab import tools
+
+TEMPLATE = '''\
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import portfolio
+{notes}
+num_configs = {num_configs}
+total_time = {total_time}
+
+CONFIGS = {portfolio}
+{final_config_source}
+{final_config_builder_source}
+portfolio.run(CONFIGS,
+              optimal={optimal},
+              final_config={final_config_name},
+              final_config_builder={final_config_builder_name})
+'''
 
 
 def _add_bound(config, optimal):
@@ -35,26 +54,29 @@ def _add_bound(config, optimal):
     return config
 
 
-def create_portfolio_script(portfolio, optimal, notes=''):
+def _pad_if_not_empty(string):
+    if string:
+        return '\n{0}\n'.format(string)
+    return ''
+
+
+def _get_name_and_source(func):
+    if func:
+        return (func.__name__, _pad_if_not_empty(inspect.getsource(func)))
+    return (None, '')
+
+
+def create_portfolio_script(portfolio, optimal, final_config=None,
+                            final_config_builder=None, notes=''):
     total_time = sum(time for time, config in portfolio)
     portfolio = [(t, _add_bound(config, optimal)) for t, config in portfolio]
-    if notes:
-        notes = '\n%s\n' % notes
-    return '''\
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import portfolio
-{notes}
-num_configs = {num_configs}
-total_time = {total_time}
-
-CONFIGS = {portfolio}
-
-portfolio.run(CONFIGS, optimal={optimal})
-'''.format(notes=notes, num_configs=len(portfolio), total_time=total_time,
-       portfolio=json.dumps(portfolio, indent=4, separators=(',', ': ')),
-       optimal=optimal)
+    portfolio = json.dumps(portfolio, indent=4, separators=(',', ': '))
+    num_configs = len(portfolio)
+    final_config_name, final_config_source = _get_name_and_source(final_config)
+    (final_config_builder_name,
+        final_config_builder_source) = _get_name_and_source(final_config_builder)
+    notes = _pad_if_not_empty(notes)
+    return TEMPLATE.format(**locals())
 
 
 def import_portfolio(path):
