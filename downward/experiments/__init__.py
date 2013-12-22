95f2d2d9894ed9ae636b004d8cf57c516795056f
Jendrik Seipp <jendrik.seipp@unibas.ch>
2013-12-22 14:54:45 +0100

    Don't delete benchmarks dirs in rev-cache.

--- downward/experiments/__init__.py
+++ downward/experiments/__init__.py
@@ -506,14 +506,6 @@ class DownwardExperiment(Experiment):
         else:
             logging.critical('There is no stage "%s"' % stage)
 
-        # Save space by deleting the benchmarks.
-        if not kwargs.get('only_main_script', False):
-            for part in sorted(translators | preprocessors | planners):
-                if part.rev != 'WORK':
-                    benchmarks = part.get_path('benchmarks')
-                    logging.info('Removing %s to save space.' % benchmarks)
-                    shutil.rmtree(benchmarks, ignore_errors=True)
-
     def _setup_ignores(self, stage):
         self.ignores = []
 
