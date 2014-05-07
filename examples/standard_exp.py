36b4d447a8a667c942f317e23e4069ebd0f6ce0a
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-05-07 11:01:45 +0200

    update cluster test

--- examples/standard_exp.py
+++ examples/standard_exp.py
@@ -15,7 +15,7 @@ from downward.reports.absolute import AbsoluteReport
 
 
 NODE = platform.node()
-REMOTE = NODE.startswith('gkigrid') or NODE.endswith('cluster') or NODE in ['habakuk', 'turtur']
+REMOTE = 'cluster' in NODE or NODE.startswith('gkigrid') or NODE in ['habakuk', 'turtur']
 SCP_LOGIN = 'seipp@maia'
 ATTRIBUTES = ['coverage', 'cost', 'quality', 'total_time']
 
