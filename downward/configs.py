134afcea9b6134bba7308c37baf8991f6d2da232
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-05-23 15:40:30 +0200

    cap long lines

--- downward/configs.py
+++ downward/configs.py
@@ -40,12 +40,14 @@ def configs_optimal_core():
             'astar(hmax)'],
         'astar_merge_and_shrink_bisim': [
             '--search',
-            'astar(merge_and_shrink(merge_strategy=merge_linear(variable_order=reverse_level),'
+            'astar(merge_and_shrink('
+            + 'merge_strategy=merge_linear(variable_order=reverse_level),'
             + 'shrink_strategy=shrink_bisimulation(max_states=200000,greedy=false,'
             + 'group_by_h=true)))'],
         'astar_merge_and_shrink_greedy_bisim': [
             '--search',
-            'astar(merge_and_shrink(merge_strategy=merge_linear(variable_order=reverse_level),'
+            'astar(merge_and_shrink('
+            + 'merge_strategy=merge_linear(variable_order=reverse_level),'
             + 'shrink_strategy=shrink_bisimulation(max_states=infinity,threshold=1,'
             + 'greedy=true,group_by_h=false)))'],
         'astar_merge_and_shrink_dfp_bisim': [
