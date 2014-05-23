49cb8b234a2b5b83ced953960a544afca2072792
Silvan Sievers <silvan.sievers@unibas.ch>
2014-05-23 14:56:08 +0200

    updated m&s configs

--- downward/configs.py
+++ downward/configs.py
@@ -40,14 +40,19 @@ def configs_optimal_core():
             'astar(hmax)'],
         'astar_merge_and_shrink_bisim': [
             '--search',
-            'astar(merge_and_shrink(merge_strategy=merge_linear_reverse_level,'
+            'astar(merge_and_shrink(merge_strategy=merge_linear(variable_order=reverse_level),'
             + 'shrink_strategy=shrink_bisimulation(max_states=200000,greedy=false,'
             + 'group_by_h=true)))'],
         'astar_merge_and_shrink_greedy_bisim': [
             '--search',
-            'astar(merge_and_shrink(merge_strategy=merge_linear_reverse_level,'
+            'astar(merge_and_shrink(merge_strategy=merge_linear(variable_order=reverse_level),'
             + 'shrink_strategy=shrink_bisimulation(max_states=infinity,threshold=1,'
             + 'greedy=true,group_by_h=false)))'],
+        'astar_merge_and_shrink_dfp_bisim': [
+            '--search',
+            'astar(merge_and_shrink(merge_strategy=merge_dfp,'
+            + 'shrink_strategy=shrink_bisimulation(max_states=50000,threshold=1,'
+            + 'greedy=false,group_by_h=true)))'],
         'astar_selmax_lmcut_lmcount': [
             '--search',
             'astar(selmax([lmcut(),lmcount(lm_merged([lm_hm(m=1),lm_rhw()]),'
