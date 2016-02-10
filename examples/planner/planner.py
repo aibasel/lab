af8678d53783e3a1182ff4e0645bd92bf0531695
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-02-10 20:56:31 +0100

    combine planner examples

--- examples/planner/planner.py
+++ examples/planner/planner.py
@@ -1,18 +1,16 @@
 #! /usr/bin/env python
 
 """
-Example downward experiment that runs FF on a single problem.
+Example downward experiment that runs h^FF and h^add on two tasks.
 
-Please adapt EXPPATH and REPO to be the path where the experiment shall be put
-and the location of your Fast Downward repository.
-
-The file planner-ext.py contains an "advanced" version of this basic experiment.
+Please adapt REPO to point to your Fast Downward repository.
 """
 
 import os.path
 
 from downward.experiment import FastDownwardExperiment
 from downward.reports.absolute import AbsoluteReport
+from downward.reports.scatter import ScatterPlotReport
 
 
 REPO = os.path.expanduser('~/projects/Downward/downward')
@@ -21,9 +19,11 @@ CACHE_DIR = os.path.expanduser('~/lab')
 
 exp = FastDownwardExperiment(cache_dir=CACHE_DIR)
 
-exp.add_suite(BENCHMARKS_DIR, 'gripper:prob01.pddl')
+exp.add_suite(BENCHMARKS_DIR, ['gripper:prob01.pddl', 'zenotravel:pfile2'])
 exp.add_algorithm('ff', REPO, 'tip', ['--search', 'lazy_greedy(ff())'])
+exp.add_algorithm('add', REPO, 'tip', ['--search', 'lazy_greedy(add())'])
 
-exp.add_report(AbsoluteReport())
+exp.add_report(AbsoluteReport(), outfile='report-abs.html')
+exp.add_report(ScatterPlotReport(attributes='expansions'))
 
 exp.run_steps()
