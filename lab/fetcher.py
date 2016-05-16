e76c36975115d57b81590dc04f90762f6358398f
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-05-16 16:13:48 +0200

    cleanup Fetcher

--- lab/fetcher.py
+++ lab/fetcher.py
@@ -25,11 +25,11 @@ from lab import tools
 
 class Fetcher(object):
     """
-    Collect data from the runs of an experiment and store it in an evaluation
-    directory.
+    Collect data from the runs of an experiment and store it in an
+    evaluation directory.
 
-    Use this class to combine data from multiple experiment or evaluation
-    directories into a (new) evaluation directory.
+    Use this class to combine data from multiple experiment or
+    evaluation directories into a (new) evaluation directory.
 
     .. note::
 
@@ -57,7 +57,7 @@ class Fetcher(object):
         run_id = props.get('id')
         # Abort if an id cannot be read.
         if not run_id:
-            logging.critical('id is not set in %s.' % prop_file)
+            logging.critical('id is not set in {}.'.format(prop_file))
 
         return run_id, props
 
@@ -75,45 +75,42 @@ class Fetcher(object):
 
         """
         if not os.path.isdir(src_dir):
-            logging.critical('%s is not a valid directory' % src_dir)
+            logging.critical('{} is missing or not a directory'.format(src_dir))
         run_filter = tools.RunFilter(filter, **kwargs)
 
         src_props = tools.Properties(filename=os.path.join(src_dir, 'properties'))
         fetch_from_eval_dir = 'runs' not in src_props or src_dir.endswith('-eval')
-        if fetch_from_eval_dir:
-            src_props = run_filter.apply(src_props)
-            for prop in src_props.values():
-                if prop.get('error', '').startswith('unexplained'):
-                    logging.warning(
-                        "Unexplained error in '%s': %s" %
-                        (prop.get('run_dir'), prop.get('error')))
 
         eval_dir = eval_dir or src_dir.rstrip('/') + '-eval'
-        logging.info('Fetching files from %s -> %s' % (src_dir, eval_dir))
-        logging.info('Fetching from evaluation dir: %s' % fetch_from_eval_dir)
+        logging.info('Fetching files from {} -> {}'.format(src_dir, eval_dir))
+        logging.info('Fetching from evaluation dir: {}'.format(fetch_from_eval_dir))
 
         # Load properties in the eval_dir if there are any already.
         combined_props = tools.Properties(os.path.join(eval_dir, 'properties'))
         if fetch_from_eval_dir:
+            src_props = run_filter.apply(src_props)
             combined_props.update(src_props)
+        else:
+            run_dirs = sorted(glob(os.path.join(src_dir, 'runs-*-*', '*')))
+            total_dirs = len(run_dirs)
+            logging.info(
+                'Scanning properties from {:d} run directories'.format(total_dirs))
+            for index, run_dir in enumerate(run_dirs, start=1):
+                loglevel = logging.INFO if index % 100 == 0 else logging.DEBUG
+                logging.log(loglevel, 'Scanning: {:06d}/{:d}'.format(index, total_dirs))
+                run_id, props = self.fetch_dir(
+                    run_dir, eval_dir, run_filter=run_filter, parsers=parsers)
+                if props is None:
+                    continue
+                if not run_id:
+                    logging.critical('Dir {run_dir} has no id'.format(**props))
+                combined_props['-'.join(run_id)] = props
 
-        # Get all run_dirs. None will be found if we fetch from an eval dir.
-        run_dirs = sorted(glob(os.path.join(src_dir, 'runs-*-*', '*')))
-        total_dirs = len(run_dirs)
-        logging.info('Scanning properties from %d run directories' % total_dirs)
         unxeplained_errors = 0
-        for index, run_dir in enumerate(run_dirs, start=1):
-            loglevel = logging.INFO if index % 100 == 0 else logging.DEBUG
-            logging.log(loglevel, 'Scanning: %6d/%d' % (index, total_dirs))
-            run_id, props = self.fetch_dir(
-                run_dir, eval_dir, run_filter=run_filter, parsers=parsers)
-            if props is None:
-                continue
-
-            assert run_id, 'Dir %s has no id' % props.get('run_dir')
-            combined_props['-'.join(run_id)] = props
+        for props in combined_props.values():
             if props.get('error', '').startswith('unexplained'):
-                logging.warning('Unexplained error in {run_dir}: {error}'.format(**props))
+                logging.warning(
+                    'Unexplained error in {run_dir}: {error}'.format(**props))
                 unxeplained_errors += 1
 
         if unxeplained_errors:
