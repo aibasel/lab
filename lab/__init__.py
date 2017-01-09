7ad7539ed03e1d359e6dcebff344d6035276e9cf
Jendrik Seipp <jendrik.seipp@unibas.ch>
2017-01-09 14:31:06 +0100

    update version number to 1.12+ after release

--- lab/__init__.py
+++ lab/__init__.py
@@ -16,4 +16,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #: lab version number. A "+" is appended to all non-tagged revisions.
-__version__ = '1.12'
+__version__ = '1.12+'
