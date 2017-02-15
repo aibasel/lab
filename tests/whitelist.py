59c0a82c826b084b9fec720cfe2008487ef80a20
Jendrik Seipp <jendrik.seipp@unibas.ch>
2017-02-15 17:37:50 +0100

    update vulture whitelist

--- tests/whitelist.py
+++ tests/whitelist.py
@@ -9,6 +9,7 @@ from lab import reports
 ARGPARSER.epilog
 reports.Table.add_col
 reports.Table.get_row
+reports.Table.set_row_order
 lab.tools.deprecated
 lab.tools.RawAndDefaultsHelpFormatter._fill_text
 lab.tools.RawAndDefaultsHelpFormatter._get_help_string
