b52ec9bbb76d7f8f5d6f3687c85be2156637fc7d
Shuwa Miura <mana_burn_vo@yahoo.co.jp>
2017-04-20 18:06:43 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email='jendrikseipp@web.de',
     url='https://bitbucket.org/jendrikseipp/lab',
     license='GPL3+',
-    py_modules=['downward', 'lab'],
+    packages=['downward', 'downward.reports','lab','lab.calls','lab.data','lab.external','lab.reports'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
