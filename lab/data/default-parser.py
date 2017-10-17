361a84e89267a109737b308d40043e9503f3383c
Jendrik Seipp <jendrik.seipp@unibas.ch>
2017-10-17 16:08:09 +0200

    test whether error logs are empty at the end of the run script

--- lab/data/default-parser.py
+++ lab/data/default-parser.py
@@ -3,26 +3,9 @@
 from lab.parser import Parser
 
 
-def check_stderr_output(content, props):
-    if content:
-        props.add_unexplained_error('output-to-run-err')
-
-
-def check_driver_stderr_output(content, props):
-    if content:
-        props.add_unexplained_error('output-to-driver-err')
-
-
-def check_driver_failures(content, props):
-    pass
-
-
 def main():
-    print "Running Lab default parser"
+    print "Running default Lab parser"
     p = Parser()
-    p.add_function(check_stderr_output, file='run.err')
-    p.add_function(check_driver_stderr_output, file='driver.err')
-    p.add_function(check_driver_failures)
     p.parse()
 
 
