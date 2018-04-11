52a94fb3757f415fbb217aeda2d9bd2547b9898c
Silvan Sievers <silvan.sievers@unibas.ch>
2018-04-11 14:58:27 +0200

    silence unused variables test

--- tests/whitelist.py
+++ tests/whitelist.py
@@ -3,6 +3,7 @@ from lab.experiment import ARGPARSER
 from lab.calls.call import Call
 from lab.calls.log import delete_file_if_empty, print_, save_returncode
 from lab import reports
+from downward.experiment import FastDownwardExperiment
 
 
 ARGPARSER.epilog
@@ -12,6 +13,7 @@ reports.Table.set_row_order
 lab.tools.deprecated
 lab.tools.RawAndDefaultsHelpFormatter._fill_text
 lab.tools.RawAndDefaultsHelpFormatter._get_help_string
+FastDownwardExperiment().PORTFOLIO_PARSER
 
 Call
 
