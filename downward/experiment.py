f5923af3b1d8e46c33813b99fc0512d35f2061fe
Silvan Sievers <silvan.sievers@unibas.ch>
2018-04-11 17:10:26 +0200

    encoding

--- downward/experiment.py
+++ downward/experiment.py
@@ -129,13 +129,13 @@ class FastDownwardExperiment(Experiment):
 
         # The following constants can be used by users to add default parsers.
         self.EXITCODE_PARSER = os.path.join(
-                DOWNWARD_SCRIPTS_DIR, 'exitcode_parser.py')
+            DOWNWARD_SCRIPTS_DIR, 'exitcode_parser.py')
         self.TRANSLATOR_PARSER = os.path.join(
-                DOWNWARD_SCRIPTS_DIR, 'preprocess_parser.py')
+            DOWNWARD_SCRIPTS_DIR, 'preprocess_parser.py')
         self.SINGLE_SEARCH_PARSER = os.path.join(
-                DOWNWARD_SCRIPTS_DIR, 'single-search-parser.py')
+            DOWNWARD_SCRIPTS_DIR, 'single-search-parser.py')
         self.PORTFOLIO_PARSER = os.path.join(
-                DOWNWARD_SCRIPTS_DIR, 'portfolio-parser.py')
+            DOWNWARD_SCRIPTS_DIR, 'portfolio-parser.py')
 
     def _get_tasks(self):
         tasks = []
