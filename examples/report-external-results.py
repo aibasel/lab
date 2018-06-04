67243088deb6ccdee1ff40dd047ebbe364a9d61d
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-06-04 19:12:23 +0200

    Make 'id' property optional for reports.

--- examples/report-external-results.py
+++ examples/report-external-results.py
@@ -5,12 +5,12 @@ Simple experiment showing how to make reports for data obtained without
 Lab.
 
 To use custom results, create the file <EXP_DIR>-eval/properties. It
-must be a JSON file mapping planner run IDs to results (see below). The
-run IDs must obviously be unique. Each value in the dictionary must
-itself be a dictionary with at least the keys "domain", "problem",
-"algorithm", "id" and "error". In addition you need the attribute names
-and values that you want to make reports for, e.g. "coverage",
-"expansions", "time".
+must be a JSON file mapping planner run names to results (see below).
+The run names must obviously be unique, but they're not used for the
+reports. Each value in the dictionary must itself be a dictionary with
+at least the keys "domain", "problem", "algorithm" and "error". In
+addition you need the attribute names and values that you want to make
+reports for, e.g. "coverage", "expansions", "time".
 
 """
 
@@ -28,7 +28,6 @@ EXP_DIR = "data/custom"
 
 PROPERTIES = {
     "ff-gripper-prob01.pddl": {
-        "id": ["ff", "gripper", "prob01.pddl"],
         "domain": "gripper",
         "problem": "prob01.pddl",
         "algorithm": "ff",
@@ -37,7 +36,6 @@ PROPERTIES = {
         "error": "plan-found",
     },
     "blind-gripper-prob01.pddl": {
-        "id": ["blind", "gripper", "prob01.pddl"],
         "domain": "gripper",
         "problem": "prob01.pddl",
         "algorithm": "blind",
@@ -56,8 +54,6 @@ def write_properties(eval_dir):
 
 # Create new experiment. The file <EXP_DIR>-eval/properties must exist.
 exp = Experiment(EXP_DIR)
-# Remove all existing experiment steps.
-exp.steps = []
 exp.add_report(AbsoluteReport(attributes=['coverage', 'expansions']))
 
 write_properties(exp.eval_dir)
