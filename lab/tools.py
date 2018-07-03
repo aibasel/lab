cd7d02efe2c5a3c502e0d21003cab7fc44d6ee20
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-07-03 12:51:54 +0200

    Filter \x00 characters from slurm.err output.

--- lab/tools.py
+++ lab/tools.py
@@ -466,6 +466,7 @@ def filter_slurm_err_content(content):
     filtered = re.sub(
         r"slurmstepd: error: task/cgroup: unable to add task\[pid=\d+\]"
         r" to memory cg '\(null\)'\n", '', content)
+    filtered = re.sub(r"\x00", '', filtered)
     return "\n".join(line for line in filtered.splitlines() if line.strip())
 
 
