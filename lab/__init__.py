c17aa636eb168889d28c2827c74671e669f73515
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-07-10 17:17:29 +0200

    update version number to 3.0 for release

--- lab/__init__.py
+++ lab/__init__.py
@@ -16,4 +16,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #: Lab version number. A "+" is appended to all non-tagged revisions.
-__version__ = '2.3+'
+__version__ = '3.0'
