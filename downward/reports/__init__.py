89688903478367742a03819620f960ff3de60ecf
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-09-16 19:03:05 +0200

    Include node name in unexplained errors table.

--- downward/reports/__init__.py
+++ downward/reports/__init__.py
@@ -209,7 +209,7 @@ class PlanningReport(Report):
         each run where an unexplained error occured.
         """
         columns = [
-            'domain', 'problem', 'algorithm', 'unexplained_errors', 'error']
+            'domain', 'problem', 'algorithm', 'unexplained_errors', 'error', 'node_name']
         table = reports.Table(title='Unexplained errors')
         table.set_column_order(columns)
 
