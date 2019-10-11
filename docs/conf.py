4784d0d77f8dfea4542d1b68a6a3cb25d7372c6e
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-10-11 14:12:37 +0200

    Fix docs by mocking matplotlib package.

--- docs/conf.py
+++ docs/conf.py
@@ -21,25 +21,7 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # -- Mock some libraries that are not available on rtfd.org --------------------
 
-class Mock(object):
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        return Mock()
-
-    @classmethod
-    def __getattr__(self, name):
-        if name in ('__file__', '__path__'):
-            return '/dev/null'
-        elif name[0] == name[0].upper():
-            return type(name, (), {})
-        else:
-            return Mock()
-
-MOCK_MODULES = ['matplotlib', 'matplotlib.backends']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = Mock()
+autodoc_mock_imports = ["matplotlib"]
 
 
 # -- General configuration -----------------------------------------------------
