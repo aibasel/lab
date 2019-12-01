5cb79f5aa6ed0b5f436a58ad52158b2102bbca9e
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-12-01 16:44:33 +0100

    List supported Python versions in setup.py file.

--- setup.py
+++ setup.py
@@ -33,13 +33,15 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering',
     ],
-    entry_points={
-    },
     install_requires=[
         'matplotlib',  # for scatter plots
         'simplejson',  # optional, speeds up reading properties files
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
 )
