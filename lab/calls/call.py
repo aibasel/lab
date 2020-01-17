33d376538fadf15dedbcf21fac5fdd74bacb4696
Jendrik Seipp <jendrik.seipp@unibas.ch>
2020-01-17 10:10:48 +0100

    Remove superfluous newline.

--- lab/calls/call.py
+++ lab/calls/call.py
@@ -205,7 +205,7 @@ class Call(object):
         if (self.wall_clock_time_limit is not None and
                 wall_clock_time > self.wall_clock_time_limit):
             logging.error(
-                'wall-clock time for %s too high: %.2f > %d\n' %
+                'wall-clock time for %s too high: %.2f > %d' %
                 (self.name, wall_clock_time, self.wall_clock_time_limit))
         logging.info('{} exit code: {}'.format(self.name, retcode))
         return retcode
