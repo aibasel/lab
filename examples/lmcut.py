8205e4e5d2ab614574eccbd033f788c329f78364
Jendrik Seipp <jendrikseipp@gmail.com>
2020-02-21 10:08:59 +0100

    Revise.

--- examples/lmcut.py
+++ examples/lmcut.py
@@ -26,7 +26,7 @@ else:
 # Use path to your Fast Downward repository.
 REPO = os.environ["DOWNWARD_REPO"]
 BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
-REVISION_CACHE = os.environ.get("LAB_REVISION_CACHE")
+REVISION_CACHE = os.environ.get("DOWNWARD_REVISION_CACHE")
 print("REVISION_CACHE:", REVISION_CACHE)
 VCS = cached_revision.get_version_control_system(REPO)
 REV = "default" if VCS == cached_revision.MERCURIAL else "master"
