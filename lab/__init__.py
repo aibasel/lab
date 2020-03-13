58b0ccb1909cbe3d24a839aaac009391f479fead
Jendrik Seipp <jendrik.seipp@unibas.ch>
2020-03-13 18:54:56 +0100

    Update version number to 5.5 for release.

--- lab/__init__.py
+++ lab/__init__.py
@@ -16,4 +16,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #: Lab version number. A "+" is appended to all non-tagged revisions.
-__version__ = "5.4+"
+__version__ = "5.5"
