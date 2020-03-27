cc31ffab5c990a25f53029a258f31bfd9a86b8d8
Jendrik Seipp <jendrik.seipp@unibas.ch>
2020-03-27 15:34:14 +0100

    Fix for Python 2.7.

--- downward/cached_revision.py
+++ downward/cached_revision.py
@@ -36,8 +36,8 @@ class CachedFastDownwardRevision(CachedRevision):
         * *local_rev*: Fast Downward revision.
         * *build_options*: List of build.py options.
         """
-        super().__init__(
-            repo, local_rev, "build.py", build_options, ["experiments", "misc"]
+        CachedRevision.__init__(
+            self, repo, local_rev, "build.py", build_options, ["experiments", "misc"]
         )
 
     def get_solver_resource_name(self):
