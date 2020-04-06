17e71cf67ea980410f9a68b4d529478301e3fd5a
Jendrik Seipp <jendrik.seipp@unibas.ch>
2020-04-06 11:45:10 +0200

    Always build docs in tox virtual environment.

--- docs/autobuild.sh
+++ docs/autobuild.sh
@@ -1,18 +1,20 @@
-#!/bin/bash
-# Automatically rebuild Sphinx documentation upon file change
+#! /bin/bash
+# Automatically rebuild Sphinx documentation when files change.
 
 DOCS="$( dirname "$0" )"
 REPO="$DOCS/../"
 
-# Make html documentation
-make -C "$DOCS" html
+cd "$REPO"
 
-# Open documentation in browser
+# Build html documentation.
+tox -e docs
+
+# Open documentation in browser.
 xdg-open "$DOCS/_build/html/index.html"
 
 while :; do
-    # Wait for changes
+    # Wait for changes.
     inotifywait -e modify,create,delete -r "$REPO"
-    # Make html documentation
-    make -C "$DOCS" html
+    # Build html documentation.
+    tox -e docs
 done
