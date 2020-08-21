992b7b7b06c9519913bf44ee514d536ece8c2746
Jendrik Seipp <jendrik.seipp@unibas.ch>
2020-08-21 16:10:53 +0200

    Remove obsolete item from whitelist.

--- tests/whitelist.py
+++ tests/whitelist.py
@@ -1,10 +1,8 @@
 import lab
 from lab import reports
 from lab.calls.call import Call
-from lab.experiment import ARGPARSER
 
 
-ARGPARSER.epilog
 reports.Table.add_col
 reports.Table.get_row
 reports.Table.set_row_order
