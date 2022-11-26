29d35fdf374ac6d2b3baf11b3e5db4ce2dbfcc9a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 23:13:49 +0000

    Adding tarfile member sanitization to extractall()

--- lab/cached_revision.py
+++ lab/cached_revision.py
@@ -126,7 +126,26 @@ class CachedRevision:
 
             if retcode == 0:
                 with tarfile.open(tar_archive) as tf:
-                    tf.extractall(self.path)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tf, self.path)
                 tools.remove_path(tar_archive)
 
                 for exclude_dir in self.exclude:
