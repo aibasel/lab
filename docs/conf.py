ac212a34e8ce15fad968988dcdf8a32f3279f21d
Jendrik Seipp <jendrik.seipp@liu.se>
2023-10-21 12:58:13 +0200

    Add readthedocs configuration file.

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "Lab"
-copyright = "2011-2020 Jendrik Seipp et al."
+copyright = "Jendrik Seipp et al."
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
