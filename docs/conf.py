535fda47f2fdc1e6e1b5f4137dd80edc259e6976
Jendrik Seipp <jendrik.seipp@liu.se>
2023-10-21 13:37:41 +0200

    Add search-as-you-type extension.

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.coverage",
     "sphinx.ext.viewcode",
+    "sphinx_search.extension",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
